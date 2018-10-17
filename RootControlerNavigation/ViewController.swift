b0ce4a44a6b022b744a59726cb7a106e8650e8a3
DavidKmn <davidk.9889@gmail.com>
2018-10-17 10:00:33 +0100

    Initial Commit

---
+++ RootControlerNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RootControlerNavigation
+//
+//  Created by David on 17/10/2018.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
